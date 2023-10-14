e4ab9b642f77533ae26b2337a4bac03d53a1534c
glaze <i@inevitable.tech>
2023-10-13 20:29:38 -0700

    update strealit app

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from icecream import ic
 import sentry_sdk
 from bs4 import BeautifulSoup
 import pandas as pd
+from classifier import classifier
+import time
 
 # sentry_sdk.init(
 #     dsn="https://fc880ea6ee11c5613ad2eb62d9eb2bf1@o262884.ingest.sentry.io/4505684111785984",
@@ -48,19 +50,50 @@ def parse(html):
 
 
 def check_keywords(x, keywords):
-    for keyword in keywords:
-        if keyword in x:
-            return True
-    return False
+    return any(keyword.lower() in x.lower() for keyword in keywords)
 
 
 def applyTag(news_df):
-    investment_keywords = ['融资', '投资', '收购', '孵化']
-    fund_keywords = ['募集', '募资', '撤资', '基金', '风险工作室']
-    news_df['tag'] = news_df['title'].apply(
-        lambda x: ['investment'] if check_keywords(x, investment_keywords) else [])
-    news_df['tag'] = news_df.apply(
-        lambda row: row['tag'] + ['fund'] if check_keywords(row['title'], fund_keywords) else row['tag'], axis=1)
+    keywords_dict = {
+        'investment': ['融资', '投资', '收购', '孵化'],
+        'fund': ['募集', '募资', '撤资', '基金', '风险工作室'],
+        'infra': ['基础设施', '上线', '主网', '测试网'],
+        'defi': ['DeFi', 'Uniswap', 'Aave', 'MakerDAO', 'compound'],
+        'nft': ['NFT', 'OpenSea', 'Rarible', 'SuperRare', 'Nifty Gateway'],
+        'wallet': ['钱包', 'Safe', 'MetaMask', 'imToken', 'Ledger', 'Trezor'],
+        'exchange': ['交易所'],
+        'regulation': ['监管', '法律', '法规', '政策', 'SEC', '诉讼']
+    }
+
+    keywords_blacklist = {
+        'investment': ['数据：'],
+        'fund': ['数据：'],
+        'infra': ['数据：'],
+        'defi': ['数据：', ],
+        'nft': ['数据：'],
+        'wallet': ['数据：'],
+        'exchange': ['数据：'],
+        'regulation': ['数据：']
+    }
+
+    news_df['tag'] = news_df['title'].apply(lambda x: [
+        tag for tag, keywords in keywords_dict.items()
+        if check_keywords(x, keywords) and not any(blackword.lower() in x.lower() for blackword in keywords_blacklist[tag])
+    ])
+    # use classfier(title) to refine investment tag
+    # for i in range(len(news_df)):
+    #     if 'investment' in news_df.iloc[i]['tag']:
+    #         new_tag = classifier(news_df.iloc[i]['title'])
+    #         if new_tag != news_df.iloc[i]['tag']:
+    #             ic(
+    #                 f"Changed tag for {news_df.iloc[i]['title']} from {news_df.iloc[i]['tag']} to {new_tag}")
+    #             news_df.iloc[i]['tag'] = [new_tag]
+    #             ic(news_df.iloc[i]['tag'], [new_tag])
+    #         # sleep 0.5s
+    #         time.sleep(0.5)
+    # save to csv
+    news_df.to_csv('news_df.csv')
+    ic("Apply Tag")
     return news_df
 
 
@@ -68,17 +101,26 @@ def main():
     st.title("每周新闻生成器（Alpha）")
     # upload file
     html = st.file_uploader("上传 PANews HTML", type="html")
-    if html is None:
-        st.stop()
-    news_df = parse(html)
-    news_df = applyTag(news_df)
-    tag_selector = st.selectbox('Select a tag', ('investment', 'fund'))
-    for i in range(len(news_df)):
-        if tag_selector in news_df.iloc[i]['tag']:
-            print(news_df.iloc[i])
-            st.markdown(
-                f"[{news_df.iloc[i]['title']}]({news_df.iloc[i]['url']})")
-            st.write(news_df.iloc[i]["content"])
+    if html is not None:
+        if 'news_df' not in st.session_state or st.session_state['html'] != html:
+            news_df = parse(html)
+            news_df = applyTag(news_df)
+            st.session_state['news_df'] = news_df
+            st.session_state['html'] = html
+        else:
+            news_df = st.session_state['news_df']
+        tag_selector = st.selectbox('Select a tag', ('investment', 'fund',
+                                    'infra', 'defi', 'nft', 'wallet', 'exchange', 'regulation'))
+        # format_string = ''
+        for i in range(len(news_df)):
+            if tag_selector in news_df.iloc[i]['tag']:
+                # print(news_df.iloc[i])
+                st.markdown(
+                    f"[{news_df.iloc[i]['title']}]({news_df.iloc[i]['url']})")
+                st.write(news_df.iloc[i]["content"])
+                # format_string += f"{news_df.iloc[i]['title']}\n"
+        # st.write(format_string)
+        # print(format_string)
 
 
 if __name__ == "__main__":
